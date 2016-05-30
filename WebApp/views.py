612e2a5da16065a1f9079e880cbe4d69831bfebe
faielomente <frlomente@up.edu.ph>
2016-05-30 11:36:52 +0800

    with working login(basta mali ang user kay stay), logout, profile page

--- WebApp/views.py
+++ WebApp/views.py
@@ -61,9 +61,6 @@ class LoginView(View):
 		user = User()
 		username = request.POST.get('student_id')
 		password = request.POST.get('password')
-		username = Student.objects.get(student_id=username)
-		print(username)
-		username = username.user_id.username
 		user = authenticate(username = username, password =password)
 		if user is not None:
 		    # the password verified for the user		
